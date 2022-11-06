3f95ea9165ca47eedd594528ab28501601e33af1
reinexxism <lucy0805@naver.com>
2022-11-06 15:52:51 +0900

    Finish Q23

---
+++ Week3/Nested Conditionals Practice.js
@@ -0,0 +1,13 @@
+const num = 102;
+
+if (num <= 100) {
+  if (num >= 50) {
+    console.log("HEY!");
+  }
+} else {
+  if (num < 103) {
+    if (num % 2 === 0) {
+      console.log("YOU GOT ME!");
+    }
+  }
+}
