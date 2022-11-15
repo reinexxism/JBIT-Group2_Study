b6da9ed562d40c143774b3f3f12d94ae4889f2d7
reinexxism <lucy0805@naver.com>
2022-11-15 20:28:35 +0900

    Finish Q30

--- Week4/Object Access Exercise.js
+++ Week4/Object Access Exercise.js
@@ -1,1 +1,10 @@
-  
+const restaurant = {
+  name: "Ichiran Ramen",
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: "Brooklyn",
+  state: "NY",
+  zipcode: "11206",
+};
+
+const fullAddress = `${restaurant.address} ${restaurant.city} ${restaurant.state} ${restaurant.zipcode}`;
+console.log(fullAddress);
