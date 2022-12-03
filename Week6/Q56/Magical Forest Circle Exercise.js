454ab7cf2fa052016e721fa13599d826b31cb1eb
reinexxism <lucy0805@naver.com>
2022-12-03 19:42:13 +0900

    Finish Q56

--- Week6/Q56/Magical Forest Circle Exercise.js
+++ Week6/Q56/Magical Forest Circle Exercise.js
@@ -2,5 +2,5 @@ const container = document.querySelector("#container");
 const img = document.querySelector("img");
 
 container.style.textAlign = "center";
-img.style.width = "150px";
+img.style.width = "200px";
 img.style.borderRadius = "50%";
