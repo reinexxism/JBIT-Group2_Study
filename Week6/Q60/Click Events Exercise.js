49eab534e2c51857d88f52d5fb505b36d5c6b6d9
reinexxism <lucy0805@naver.com>
2022-12-03 20:20:35 +0900

    Finish Q60

---
+++ Week6/Q60/Click Events Exercise.js
@@ -0,0 +1,9 @@
+const helloButton = document.querySelector("#hello");
+const goodbyeButton = document.querySelector("#goodbye");
+
+helloButton.addEventListener("click", () => {
+  console.log("hello");
+});
+goodbyeButton.addEventListener("click", () => {
+  console.log("goodbye");
+});
